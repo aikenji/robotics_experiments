f5a30821e31660ccf5ba904205f71f687a4d0315
aikenji <aikenjch@gmail.com>
2024-04-14 12:02:27 +0800

    add more so3 codes

--- experiments/se2.py
+++ experiments/se2.py
@@ -5,14 +5,14 @@ T = SE2(1, 1, 60, unit="deg")
 T_1 = SE2(0, 0, 30, unit="deg")
 T_0 = SE2(0, 0, 0, unit="deg")
 p = [1, 0]
-lim = [-3, 3, -3, 3]
+lim = [-1, 3, -1, 3]
 
 # T_i = T.inv()
 # T_i.print()
 pp = T_1 * p
 print(pp)
 
-T.animate(frame="B", color="red", width=1, dims=lim, repeat=True)
+T.animate(frame="B", color="green", width=1, dims=lim, repeat=True)
 T.plot(frame="B", color="red", width=1, dims=lim)
 T_0.plot(frame="A", color="blue", width=1, dims=lim)
 
